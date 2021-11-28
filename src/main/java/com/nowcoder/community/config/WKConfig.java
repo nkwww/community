e5460792b253ae8df37eea8bdcee414ed010c5b0
18079206114 <492904099@qq.com>
2021-11-28 17:52:11 +0800

    feat: 完成生成长图API

---
+++ src/main/java/com/nowcoder/community/config/WKConfig.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Configuration
+public class WKConfig {
+    private static final Logger logger = LoggerFactory.getLogger(WKConfig.class);
+
+    @Value("${wk.image.storage}")
+    private String WKImageStorage;
+
+    @PostConstruct
+    public void init() {
+        // 创建WK图片目录
+        File file = new File(WKImageStorage);
+        if (!file.exists()) {
+            file.mkdir();
+            logger.info("创建图片目录： " + WKImageStorage);
+        }
+    }
+}
---
+++ src/main/java/com/nowcoder/community/controller/ShareController.java
@@ -0,0 +1,91 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Event;
+import com.nowcoder.community.event.EventProducer;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class ShareController implements CommunityConstant {
+    private static final Logger logger = LoggerFactory.getLogger(ShareController.class);
+
+    // 生成图片时间比较长，采用异步的方式，把HtmlToImage 的操作交给Kafka消息队列后 异步执行
+    // 避免请求的时间过长
+    @Autowired
+    private EventProducer eventProducer;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${wk.image.storage}")
+    private String wkImageStorage;
+
+    @RequestMapping(path = "/share", method = RequestMethod.GET)
+    @ResponseBody
+    public String share(String htmlUrl) {
+        // 生成图片的文件名
+        String fileName = CommunityUtil.generateUUID();
+
+        // 异步生成长图
+        // 构建事件
+        Event event = new Event();
+        event.setTopic(TOPIC_SHARE)
+                .setData("htmlUrl", htmlUrl)
+                .setData("fileName", fileName)
+                .setData("suffix", ".png");
+
+        // 触发事件
+        eventProducer.fireEvent(event);
+
+        // 返回长图访问路径
+        Map<String, Object> map = new HashMap<>();
+        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+        return CommunityUtil.getJsonString(0, null, map);
+    }
+
+    @RequestMapping(path = "/share/image/{fileName}", method = RequestMethod.GET)
+    public void getShareImage(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        if (StringUtils.isBlank(fileName)) {
+            throw new IllegalArgumentException("文件名不能为空！");
+        }
+        response.setContentType("image/png");
+
+        // 使用IO流返回图像给浏览器
+        // 服务器存放路径
+        fileName = wkImageStorage + "/" + fileName + ".png";
+        // 响应图片
+        try (
+                FileInputStream fileInputStream = new FileInputStream(fileName);
+                OutputStream outputStream = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fileInputStream.read(buffer)) != -1) {
+                outputStream.write(buffer,0,b);
+            }
+        } catch (IOException e) {
+            logger.error("读取长图失败!" + e.getMessage());
+        }
+    }
+
+}
